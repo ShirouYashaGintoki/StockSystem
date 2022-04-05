99cd2389c970400d22b012a2416c7d561e94a2ed
JJosuke <projectyuni5@gmail.com>
2022-04-05 04:38:47 +0100

    Updated some tests

--- getSignalDates.py
+++ getSignalDates.py
@@ -16,6 +16,23 @@ headers = {
     'x-rapidapi-key': "d9d76c3270msh16a19417bd4b485p1b0395jsn955227be6f56"
     }
 
-df = pd.read_excel('tickers2.xlsx', sheet_name='Sheet 1')
+indices = pd.read_excel('tickers2.xlsx', sheet_name='Sheet 2')
 
-tickers = df['Symbol']
+tickers = indices['Symbol']
+
+linecount = 0
+
+for ticker in tickers:
+    print(ticker, end=" ")
+    linecount += 1
+    if linecount % 5 == 0:
+        time.sleep(60)
+    try:
+        querystring = {"symbol":ticker,"interval":"1h","outputsize":"1","format":"json"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        jsonResponse = response.json()
+        df2 = json_normalize(jsonResponse, 'values')
+        print(df2)
+        # print(jsonResponse)
+    except Exception as e:
+        print(ticker + " " + str(e))
