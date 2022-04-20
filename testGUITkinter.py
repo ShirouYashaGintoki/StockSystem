e8e0d9217b7232a6ee37ed82b3d3831858dc4281
JJosuke <projectyuni5@gmail.com>
2022-04-20 05:08:12 +0100

    Keep fixing stuff

--- testGUITkinter.py
+++ testGUITkinter.py
@@ -7,55 +7,67 @@ indices = pd.read_excel('tickers2.xlsx', sheet_name='Sheet 1')
 # Tickers is a list of symbols as strings from the 'Symbol' column
 # of the dataframe 'indices'
 tickers = sorted(indices['Symbol'])
+timeFrames = ['HOURLY', 'DAILY', 'WEEKLY']
 
-print(tickers)
 setTickers = ['', '', '', '', '']
+stockRotations = [
+     ['', ''],
+     ['', ''],
+     ['', ''],
+     ['', ''],
+     ['', '']
+]
 
 root = Tk()
 root.title("Simple Stock Signal System")
 root.geometry("750x720")
 root.resizable(False, False)
 
-clicked1 = StringVar(root)
+clicked1   = StringVar(root)
+clicked2   = StringVar(root)
+clicked3   = StringVar(root)
+clicked4   = StringVar(root)
+clicked5   = StringVar(root)
+timeFrame1 = StringVar(root)
 
-def callback(*args):
-     if setTickers.count(clicked1.get()) >= 1:
-          print(f'The variable {clicked1.get()} already exists')
-     
-     print(f"the variable has changed to '{clicked1.get()}'")
+def callback1(*args):
+     pass
 
 clicked1.set(tickers[0])
+setTickers[0] = clicked1.get()
+stockRotations[0][0] = clicked1.get()
+clicked1.trace("w", callback1)
+timeFrame1.set(timeFrames[0])
+
+clicked2.set(tickers[1])
+setTickers[1] = clicked2.get()
+stockRotations[1][0] = clicked2.get()
 print(setTickers)
-clicked1.trace("w", callback)
-
-
-clicked2 = StringVar(root)
-clicked2.set(tickers[0])
-
-clicked3 = StringVar(root)
-clicked3.set(tickers[0])
-
-clicked4 = StringVar(root)
-clicked4.set(tickers[0])
-
-clicked5 = StringVar(root)
-clicked5.set(tickers[0])
+# clicked2.trace("w", callback1)
+clicked3.set(tickers[2])
+clicked4.set(tickers[3])
+clicked5.set(tickers[4])
 
 
 drop1 = OptionMenu(root, clicked1, *tickers)
-drop1.config(width=20, bg="green", foreground="white")
-drop1.grid(row=0, column=0)
+drop1.config(width=22, bg="green", foreground="white")
+drop1.place(x=0, y=0)
 
 button1 = Button(root, text="Get chart")
 button1.columnconfigure(0, weight=0)
-button1.grid(row=1, column=0, sticky=W)
+button1.place(x=0, y=35)
 
-# drop2 = OptionMenu(root, clicked2, *listOfIndices)
-# drop2.config(width=20, bg="green", foreground="white")
-# drop2.grid(row=2, column=0)
-# button2 = Button(root, text="Get chart")
-# button2.columnconfigure(0, weight=0)
-# button2.grid(row=3, column=0, sticky=W)
+dropTf1 = OptionMenu(root, timeFrame1, *timeFrames)
+dropTf1.config(width=10, bg="blue", foreground="white")
+dropTf1.place(x=72, y=32)
+
+drop2 = OptionMenu(root, clicked2, *tickers)
+drop2.config(width=20, bg="green", foreground="white")
+drop2.place(x=0, y=80)
+
+button2 = Button(root, text="Get chart")
+button2.columnconfigure(0, weight=0)
+button2.place(x=0, y=110)
 
 # drop3 = OptionMenu(root, clicked3, *listOfIndices)
 # drop3.config(width=20, bg="green", foreground="white")
