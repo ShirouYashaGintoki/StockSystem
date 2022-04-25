ebd5403b90a47358f616aaf1bc8b9e6164bf673c
JJosuke <projectyuni5@gmail.com>
2022-04-25 04:26:03 +0100

    Added timer script using threading

--- testGUITkinter.py
+++ testGUITkinter.py
@@ -2,6 +2,35 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import scrolledtext as st
 import pandas as pd
+import threading
+import time
+
+class RepeatedTimer(object):
+  def __init__(self, interval, function, *args, **kwargs):
+    self._timer = None
+    self.interval = interval
+    self.function = function
+    self.args = args
+    self.kwargs = kwargs
+    self.is_running = False
+    self.next_call = time.time()
+    self.start()
+
+  def _run(self):
+    self.is_running = False
+    self.start()
+    self.function(*self.args, **self.kwargs)
+
+  def start(self):
+    if not self.is_running:
+      self.next_call += self.interval
+      self._timer = threading.Timer(self.next_call - time.time(), self._run)
+      self._timer.start()
+      self.is_running = True
+
+  def stop(self):
+    self._timer.cancel()
+    self.is_running = False
 
 # Indices as dataframe, Sheet 1 is main sheet, Sheet 2 has 5 for testing
 indices = pd.read_excel('tickers2.xlsx', sheet_name='Sheet 1')
@@ -118,6 +147,7 @@ def callback2(clicker, timeframe, clickerName):
                
           print(f'drop variable has been changed to {timeframe.get()}')
           print([clicker.get(), timeframe.get()])
+
 
 # Stock 1
 #####################################
@@ -266,5 +296,11 @@ displayBox.place(x=300, y=2)
 # displayBox.delete("1.0","end")
 displayBox.configure(state="disabled")
 
+def hello(name):
+     print(f'hello {name}')
+
+rt = RepeatedTimer(1, hello, "beaner")
 
 root.mainloop()
+
+rt.stop()
