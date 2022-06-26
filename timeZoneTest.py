d395411657d746207b6b90fd726717f827ef1b7d
JJosuke <projectyuni5@gmail.com>
2022-06-26 01:07:40 +0100

    Fixed tz, implement and test conf needed

--- timeZoneTest.py
+++ timeZoneTest.py
@@ -1,5 +1,20 @@
-import pandas as pd
-import datetime as dtOver
-from datetime import datetime as dtInner
+from datetime import datetime
 from dateutil import tz
-
+import pytz
+# dt_str  = "10/21/2021 8:18:19"
+other_string = '2022-06-22 15:55:00'
+format = "%Y-%m-%d %H:%M:%S"
+ukFormat = "%d-%m-%Y %H:%M:%S"
+# format = "%d%m%Y %H:%M:%S"
+# Create datetime object in local timezone
+# dt_utc = datetime.strptime(dt_str, format)
+dt_utc = datetime.strptime(other_string, format)
+dt_utc = dt_utc.replace(tzinfo=pytz.UTC)
+print('Datetime in UTC Time zone: ', dt_utc)
+# Get local timezone
+local_zone = tz.tzlocal()
+# Convert timezone of datetime from UTC to local
+dt_local = dt_utc.astimezone(local_zone)
+print('Datetime in Local Time zone: ', dt_local)
+local_time_str = dt_local.strftime(ukFormat)
+print('Time as string in Local Time zone: ', local_time_str)
