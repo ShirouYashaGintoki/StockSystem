ce39b05984cfec188f745a401af605557e19fbe1
JJosuke <projectyuni5@gmail.com>
2022-06-30 00:17:11 +0100

    testing chart

--- nonBlockChart.py
+++ nonBlockChart.py
@@ -2,7 +2,40 @@ import pandas as pd
 from pandas import json_normalize
 import requests
 from tabulate import tabulate
+import plotly.express as px
+import plotly.graph_objects as go
+import time
+import threading
 
+def printSomething():
+    print("This should happen in the background")
+
+class RepeatedTimer(object):
+     def __init__(self, interval, function, *args, **kwargs):
+          self._timer = None
+          self.interval = interval
+          self.function = function
+          self.args = args
+          self.kwargs = kwargs
+          self.is_running = False
+          self.next_call = time.time()
+          self.start()
+
+     def _run(self):
+          self.is_running = False
+          self.start()
+          self.function(*self.args, **self.kwargs)
+
+     def start(self):
+          if not self.is_running:
+               self.next_call += self.interval
+               self._timer = threading.Timer(self.next_call - time.time(), self._run)
+               self._timer.start()
+               self.is_running = True
+
+     def stop(self):
+          self._timer.cancel()
+          self.is_running = False
 
 # URL for API
 url = "https://twelve-data1.p.rapidapi.com/time_series"
