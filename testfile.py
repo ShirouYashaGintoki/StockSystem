76589465011abec2ba6d07de3390b153f6084382
JJosuke <projectyuni5@gmail.com>
2022-07-14 00:16:43 +0100

    cont

--- testfile.py
+++ testfile.py
@@ -1,9 +1,14 @@
-first_number = input("Enter your first number :")
-second_number = input("Enter your second number :")
+import yfinance as yf
+from datetime import datetime, timedelta
 
+days = datetime.timedelta(1)
+start = datetime.today().strftime('%Y-%m-%d') - days
+end = datetime.today().strftime('%Y-%m-%d')
+tsla = yf.ticker('tsla')
+tslahist = tsla.history(period='1mo', interval='5min', start=start, end=end, auto_adjust=False, rounding=True)
 
-percent_diff = (((float(second_number) - float(first_number))/float(first_number)) * 100)
-prefix = "+" if percent_diff > 0 else ''
-print(f'Difference is {prefix}{percent_diff:.2f}%')
+# percent_diff = (((float(second_number) - float(first_number))/float(first_number)) * 100)
+# prefix = "+" if percent_diff > 0 else ''
+# print(f'Difference is {prefix}{percent_diff:.2f}%')
 
 # Use alpha x2 to do mover updates every 30 mins/hour
