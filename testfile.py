b3545f7e6f099f9465eee60c27bcc39100a68d23
Taahaa <58344496+JJosuke@users.noreply.github.com>
2022-07-14 16:48:00 +0100

    cont

--- testfile.py
+++ testfile.py
@@ -1,11 +1,15 @@
 import yfinance as yf
-from datetime import datetime, timedelta
+import datetime
 
-days = datetime.timedelta(1)
-start = datetime.today().strftime('%Y-%m-%d') - days
-end = datetime.today().strftime('%Y-%m-%d')
-tsla = yf.ticker('tsla')
-tslahist = tsla.history(period='1mo', interval='5min', start=start, end=end, auto_adjust=False, rounding=True)
+tod = datetime.datetime.now()
+d = datetime.timedelta(days = 1)
+a = tod - d
+a = a.strftime("%d-%m-%Y")
+tod = tod.strftime("%d-%m-%Y")
+print(f'Today: {tod} -> Yesterday: {a}')
+
+# tsla = yf.ticker('tsla')
+# tslahist = tsla.history(period='1mo', interval='5min', start=start, end=end, auto_adjust=False, rounding=True)
 
 # percent_diff = (((float(second_number) - float(first_number))/float(first_number)) * 100)
 # prefix = "+" if percent_diff > 0 else ''
