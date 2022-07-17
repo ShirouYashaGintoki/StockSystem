53a87b58b67fed9a4f0b9698d95ee6fcabd44377
JJosuke <projectyuni5@gmail.com>
2022-07-18 00:49:23 +0100

    yfinance done, implement and test

--- ProjectWorkingFolder/testfile.py
+++ ProjectWorkingFolder/testfile.py
@@ -2,17 +2,25 @@ import yfinance as yf
 import datetime
 
 tod = datetime.datetime.now()
-d = datetime.timedelta(days = 1)
+d = datetime.timedelta(days = 7)
 a = tod - d
-a = a.strftime("%d-%m-%Y")
-tod = tod.strftime("%d-%m-%Y")
+start = a.strftime("%Y-%m-%d")
+end = tod.strftime("%Y-%m-%d")
+# start = a.strftime("%d-%m-%Y")
+# end = tod.strftime("%d-%m-%Y")
 print(f'Today: {tod} -> Yesterday: {a}')
 
-# tsla = yf.ticker('tsla')
-# tslahist = tsla.history(period='1mo', interval='5min', start=start, end=end, auto_adjust=False, rounding=True)
+apple= yf.Ticker("aapl")
+applehist = apple.history(period='1wk', interval='1d', start=start, end=end, auto_adjust=False, rounding=True)
+applehist = applehist.iloc[::-1]
+applehist.drop(['Dividends', 'Adj Close', 'Stock Splits'], axis=1, inplace=True)
+print(applehist)
 
-# percent_diff = (((float(second_number) - float(first_number))/float(first_number)) * 100)
-# prefix = "+" if percent_diff > 0 else ''
-# print(f'Difference is {prefix}{percent_diff:.2f}%')
+day1Close = applehist.Close.iloc[0]
+day2Close = applehist.Close.iloc[1]
 
-# Use alpha x2 to do mover updates every 30 mins/hour
+print(day1Close, day2Close)
+percent_diff = (((float(day1Close) - float(day2Close))/float(day2Close)) * 100)
+prefix = "+" if percent_diff > 0 else ''
+print(f'Difference is {prefix}{percent_diff:.2f}%')
+
